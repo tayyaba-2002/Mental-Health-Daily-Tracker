dbe152a7e5a1238b0f7f299444dd29c9ef1c2840
Tayyaba Shakeel <tshakeel871@gmail.com>
2025-08-06 13:45:33 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,24 @@
-const CACHE_NAME = "daily-tracker-cache-v1";
+const CACHE_NAME = 'mental-health-tracker-v1';
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/chart.js"
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json',
+  '/screenshot-desktop.png',
+  '/screenshot-mobile.png'
 ];
 
-// Install service worker
-self.addEventListener("install", (event) => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch requests
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
